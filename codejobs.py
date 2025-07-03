3b5d030abe0bb11ace98acc5aae8baef2ffd819f
sandesh2057 <sandeshbhattarai6@gmail.com>
2025-07-03 16:20:06 +0545

    last changes for logo size

--- codejobs.py
+++ codejobs.py
@@ -57,7 +57,7 @@ col1, col2 = st.columns([7, 1])
 
 with col1:
     st.markdown(
-        "<img src='https://raw.githubusercontent.com/sandeshbhattarai101/CodeJobsNepal/main/codejobslogo.png' style='height: 7em;'>",
+        "<img src='https://raw.githubusercontent.com/sandeshbhattarai101/CodeJobsNepal/main/codejobslogo.png' style='height: 6em;'>",
         unsafe_allow_html=True
     )
 with col2:
